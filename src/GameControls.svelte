d4c68c7967d85ac0e652ce9071654f820a9e7b31
Kristjan Pekk <kristjan.pekk@cyber.ee>
2023-05-15 13:48:11 +0300

    moved gamecontrols to separate component

--- src/GameControls.svelte
+++ src/GameControls.svelte
@@ -1,24 +1,60 @@
 <script>
-    export let height;
+  import { createEventDispatcher } from "svelte";
+  const dispatch = createEventDispatcher();
+
+  export let gameSpeed = 1;
+
+  const startGame = () => {
+    dispatch("startGame");
+  };
+
+  const stopGame = () => {
+    dispatch("stopGame");
+  };
+
+  const resetField = () => {
+    dispatch("resetField");
+  };
+
+  $: sliderLabel = `Game speed: <span style="color: var(--primary);">${gameSpeed}</span>
+      ${gameSpeed === 1 ? "move" : "moves"} per second`;
 </script>
 
-<div class="controls-container" style="height:{height}">
-    <div class="buttons">
-        <button>Start</button>
-        <button>Pause</button>
-        <button>Reset</button>
-    </div>
+<div class="controls-container">
+  <div class="buttons">
+    <button on:click={startGame}>Start</button>
+    <button on:click={stopGame}>Stop</button>
+    <button on:click={resetField}>Reset</button>
+  </div>
+  <div class="slider-container">
+    <p class="slider-label">
+      {@html sliderLabel}
+    </p>
+    <input type="range" min="1" max="10" bind:value={gameSpeed} />
+  </div>
 </div>
 
 <style>
-    .controls-container {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-    }
-    .buttons {
-        width: 40%;
-        display: flex;
-        justify-content: space-around;
-    }
+  .controls-container {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    margin-top: 5rem;
+  }
+
+  .slider-container {
+    align-items: center;
+    background-color: var(--dark-purple);
+    border-radius: 2rem;
+
+    width: 32rem;
+    margin-top: 1rem;
+    padding: 1rem;
+  }
+
+  .slider-label {
+    color: var(--secondary);
+    font-size: 2rem;
+  }
 </style>
